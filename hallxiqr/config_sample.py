c50b62226adeb719fd49268954a0d2bfe6aebc0c
Wei Xing <weixingp5@gmail.com>
2021-01-08 20:02:58 +0800

    init

---
+++ hallxiqr/config_sample.py
@@ -0,0 +1,7 @@
+# Sample config file, please fill and rename to config.py
+
+# Run the following to generate with python console
+# from django.core.management.utils import get_random_secret_key
+# print(get_random_secret_key())
+
+django_secret_key = ""
