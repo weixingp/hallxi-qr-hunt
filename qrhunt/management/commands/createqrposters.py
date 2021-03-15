f6af9a6c618cf1c7f7e7f806b6e5c0ae04b85529
Wei Xing <weixingp5@gmail.com>
2021-03-15 19:58:06 +0800

    added qr generator

--- qrhunt/management/commands/createqrposters.py
+++ qrhunt/management/commands/createqrposters.py
@@ -9,23 +9,26 @@ from qrhunt.models import Location
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # locations = Location.objects.all()
         google_qr_base = "https://chart.googleapis.com/chart"
-        payload = {
-            "cht": "qr",
-            "chs": "500x500",
-            "chld": "M|0",
-            "chl": "https://127.0.0.1:8000/location/24d8f616-e736-4a11-b39f-0785904dd593"
-        }
         font_dir = str(BASE_DIR) + '/qrhunt/qrcodes/Roboto-Regular.ttf'
-        # font = ImageFont.truetype(str(BASE_DIR) + '/qrhunt/qrcodes/Roboto-Regular.ttf', size=12)
         font = ImageFont.truetype(font_dir, size=47)
 
-        qr = Image.open(requests.get(google_qr_base, stream=True, params=payload).raw)
-        qr = qr.resize((984, 984))
-        im = Image.open(BASE_DIR / 'qrhunt/qrcodes/base.jpg')
-        im.paste(qr, (1369, 811))
+        locations = Location.objects.all()
+        for location in locations:
+            location_uuid = location.uuid_str()
+            payload = {
+                "cht": "qr",
+                "chs": "500x500",
+                "chld": "M|0",
+                "chl": f"https://play.hallxi.com/location/{location_uuid}"
+            }
 
-        draw = ImageDraw.Draw(im)
-        draw.text((1600, 1822), "Block 55 Lounge", (255, 255, 255), font=font)
-        im.save(BASE_DIR / 'qrhunt/qrcodes/generated/test.jpg')
+            qr = Image.open(requests.get(google_qr_base, stream=True, params=payload).raw)
+            qr = qr.resize((984, 984))
+            im = Image.open(BASE_DIR / 'qrhunt/qrcodes/base.jpg')
+            im.paste(qr, (1369, 811))
+
+            draw = ImageDraw.Draw(im)
+            draw.text((1600, 1822), "Block 55 Lounge", (255, 255, 255), font=font)
+            save_dir = str(BASE_DIR) + f"qrhunt/qrcodes/generated/{location.uuid_str()}.jpg"
+            im.save(save_dir)
