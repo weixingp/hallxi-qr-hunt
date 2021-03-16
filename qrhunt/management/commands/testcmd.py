b8001d1a432fa58affe7d6c59eb0731fe44ea9cd
Wei Xing <weixingp5@gmail.com>
2021-03-16 22:57:53 +0800

    added test cmd

---
+++ qrhunt/management/commands/testcmd.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+import sys
+from PIL import Image, ImageFont, ImageDraw
+import requests
+from django.utils.timezone import localtime
+
+from hallxiqr.settings import BASE_DIR
+from qrhunt.models import Location, AssignedQuestion
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.get(id=4)
+
+        question_list = AssignedQuestion.objects.filter(
+            user=user,
+            has_answered=False,
+            time__date=localtime().date(),
+        )
+
+        print(question_list)
