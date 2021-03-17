0d6c207e9920a3725f0582b661adf6bb985ac8cb
Wei Xing <weixingp5@gmail.com>
2021-03-17 13:13:25 +0800

    added sms command

---
+++ qrhunt/management/commands/sendwelcomesms.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from qrhunt.apis.Twilio import Twilio
+from qrhunt.models import Profile, AssignedLootBox
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        profiles = Profile.objects.all()
+        profiles = [Profile.objects.get(pk=3)]
+        count = 1
+        total = len(profiles)
+        body = "Hi Hall XI, phase 2 of IBG has begun. Launch the app https://play.hallxi.com to play now. $500 " \
+               "Shopee voucher, 24inch monitor and JBL speakers to be won."
+
+        for profile in profiles:
+            print(f"Processing SMS: {count} / {total}")
+            user = profile.user
+
+            if not profile.mobile:
+                continue
+
+            hp = profile.mobile
+
+            tw = Twilio()
+            tw.send(hp, body)
