43164f5b8dfff46b682cba56372923d13af32b7d
Wei Xing <weixingp5@gmail.com>
2021-03-17 11:38:50 +0800

    updated

--- qrhunt/management/commands/assignlootbox.py
+++ qrhunt/management/commands/assignlootbox.py
@@ -1,17 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
-from qrhunt.models import Location, Profile, AssignedLocation, AssignedLootBox
-from random import sample
+from qrhunt.models import Profile, AssignedLootBox
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('amount', nargs='+', type=int)
-        parser.add_argument('reason', nargs='+', type=str)
+        parser.add_argument('-n', "--amount", type=int)
+        parser.add_argument('-r', "--reason", type=str)
 
     def handle(self, *args, **options):
+        if not options["amount"] or not options["reason"]:
+            raise ValueError("Invalid args.")
+
         n = options["amount"]
         profiles = Profile.objects.all()
-        profiles = [Profile.objects.get(pk=3)]
         for profile in profiles:
             user = profile.user
 
