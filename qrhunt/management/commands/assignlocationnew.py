07dd0a375791cb134063c5950befc9997877f55c
Wei Xing <weixingp5@gmail.com>
2021-03-17 13:44:34 +0800

    added assignlocation for new players

---
+++ qrhunt/management/commands/assignlocationnew.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from qrhunt.models import Location, Profile, AssignedLocation
+from random import sample
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        profiles = Profile.objects.all()
+        n = 5
+        for profile in profiles:
+            user = profile.user
+
+            has_location = AssignedLocation.objects.filter(
+                user=user
+            )
+
+            if has_location:
+                continue
+            else:
+                print(f"Assigning location for new player: {profile.fullname}")
+
+                pks = Location.objects.filter(
+                    area__in=[profile.block.name, "00"],
+                ).values_list('pk', flat=True)
+
+                if len(pks) < n:
+                    raise ValueError("Not enough locations")
+
+                locations = sample(list(pks), k=n)
+                for location_id in locations:
+                    location = Location.objects.get(pk=location_id)
+                    AssignedLocation.objects.create(
+                        user=user,
+                        location=location
+                    )
